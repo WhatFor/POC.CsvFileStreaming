1ba547af2b950dc85d195c300713c681bb03d0c6
Chris Walker <chris@awsm.tech>
2023-08-07 16:39:59 +0100

    WIP

---
+++ POC.CsvFileStreaming/Controllers/DownloadController.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+using CsvHelper;
+using Microsoft.AspNetCore.Mvc;
+using POC.CsvFileStreaming.Services;
+
+namespace POC.CsvFileStreaming.Controllers;
+
+[ApiController]
+[Route("Download")]
+public class DownloadController : ControllerBase, IDisposable
+{
+    private readonly FileContentService _fileContentService;
+
+    public DownloadController(IConfiguration config)
+    {
+        // Bad - use DI in the real world :) This is just a POC
+        _fileContentService = new FileContentService(config);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> IndexAsync()
+    {
+        var textWriter = new StreamWriter(new MemoryStream());
+        var csvWriter = new CsvWriter(textWriter, CultureInfo.CurrentCulture);
+        
+        // write header
+        csvWriter.WriteHeader<Model>();
+
+        var rows = _fileContentService.GetData();
+        
+        await csvWriter.WriteRecordsAsync(rows);
+        await textWriter.FlushAsync();
+        
+        //reset to beginning of stream
+        textWriter.BaseStream.Seek(0, SeekOrigin.Begin);
+        
+        Console.WriteLine($"Returning file stream at {DateTime.Now:hh:mm:ss.fff}");
+        return new FileStreamResult(textWriter.BaseStream, "text/csv")
+        {
+            FileDownloadName = "data.csv"
+        };
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine($"disposing controller at {DateTime.Now:hh:mm:ss.fff}");
+        _fileContentService.Dispose();
+    }
+}
---
+++ POC.CsvFileStreaming/Services/FileContentService.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace POC.CsvFileStreaming.Services;
+
+public class FileContentService : IDisposable
+{
+    private readonly string connectionString;
+    private readonly SqlConnection _sqlConnection;
+    
+    private const string SqlQuery = "SELECT * FROM MOCK_DATA";
+
+    public FileContentService(IConfiguration config)
+    {
+        connectionString = config.GetConnectionString("Default")!;
+        _sqlConnection = new SqlConnection(connectionString);
+    }
+    
+    public async IAsyncEnumerable<Model> GetData()
+    {
+        await foreach (var row in _sqlConnection.QueryUnbufferedAsync<Model>(SqlQuery))
+        {
+            yield return row;
+        }
+    }
+
+    public void Dispose()
+    {
+        _sqlConnection.Dispose();
+    }
+}
+
+public class Model
+{
+    public int Id { get; set; }
+    
+    public string? First_Name { get; set; }
+    
+    public string? Last_Name { get; set; }
+    
+    public string? Email { get; set; }
+    
+    public string? Gender { get; set; }
+    
+    public string? Ip_Address { get; set; }
+}
